a4cc606f58f8a1b9d4795a8a331562c091e8e8e1
Walter Maldonado Jr <walter@agroestat.com.br>
2018-05-30 12:08:41 -0300

    Added options to save or show processing steps

--- ccov.py
+++ ccov.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 
 
-def carcass_coverage(image_path):
+def carcass_coverage(image_path, show_steps=False, save_steps=False):
 
     # Retrieving original image
     img = cv2.imread(image_path, cv2.IMREAD_COLOR)
@@ -22,47 +22,34 @@ def carcass_coverage(image_path):
     lower_color_hang1 = np.array((0, 0, 0), dtype=np.uint8, ndmin=1)
     upper_color_hang1 = np.array((180, 255, 55), dtype=np.uint8, ndmin=1)
     mask_hang1 = cv2.inRange(img_hsv, lower_color_hang1, upper_color_hang1)
-    mask_hang_hsv1 = cv2.cvtColor(mask_hang1, cv2.COLOR_GRAY2BGR)
 
-    img_hang = img & mask_hang_hsv1
-    _, img_hang = cv2.threshold(img_hang, 2, 255, cv2.THRESH_BINARY)
-    img_hang = cv2.cvtColor(img_hang, cv2.COLOR_BGR2GRAY)
     structuring_el = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    dilation = cv2.dilate(img_hang, structuring_el, iterations=3)
+    dilation_hang = cv2.dilate(mask_hang1, structuring_el, iterations=3)
     # End: HANG PIXELS RANGE
 
     # Start: TOTAL PIXELS RANGE
     lower_color_total = np.array((0, 30, 30), dtype=np.uint8, ndmin=1)
     upper_color_total = np.array((255, 255, 255), dtype=np.uint8, ndmin=1)
     mask_total = cv2.inRange(img_hsv, lower_color_total, upper_color_total)
-    mask_total_hsv = cv2.cvtColor(mask_total, cv2.COLOR_GRAY2BGR)
-
-    img_total_range = img & mask_total_hsv
-
-    img_total_gray = cv2.cvtColor(img_total_range, cv2.COLOR_BGR2GRAY)
-    _, th_img_total_gray = cv2.threshold(img_total_gray, 20, 255, cv2.THRESH_BINARY)
 
     # Background and hang removal from the total area
-    th_img_total_gray = th_img_total_gray - dilation - mask_bg1
-    _, th_img_total_gray = cv2.threshold(th_img_total_gray, 20, 255, cv2.THRESH_BINARY)
+    img_total_gray = mask_total - dilation_hang - mask_bg1
+    _, img_total_gray = cv2.threshold(img_total_gray, 20, 255, cv2.THRESH_BINARY)
 
     structuring_el = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    opening_img_total_gray = cv2.morphologyEx(th_img_total_gray, cv2.MORPH_OPEN, structuring_el, iterations=1)
+    opening_img_total_gray = cv2.morphologyEx(img_total_gray, cv2.MORPH_OPEN, structuring_el, iterations=1)
     structuring_el = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
     closing_img_total_gray = cv2.morphologyEx(opening_img_total_gray, cv2.MORPH_CLOSE, structuring_el, iterations=3)
 
-    cntrs = []
+    selected_contours = []
     _, contours, hierarchy = cv2.findContours(closing_img_total_gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     for contour in contours:
         if cv2.contourArea(contour) > 20000:
-            cntrs.append(contour)
+            selected_contours.append(contour)
 
-    if len(cntrs) > 0:
+    if len(selected_contours) > 0:
         epsilon = 5
-        approx = cv2.approxPolyDP(cntrs[0], epsilon, True)
-
-        # Step-by-step image processing description. TODO: Remove after tests
-        # cv2.drawContours(img_total_range, [approx], -1, (0, 255, 0), 3)
+        approx = cv2.approxPolyDP(selected_contours[0], epsilon, True)
 
         img_total = np.zeros((640, 480), dtype=np.uint8)
         cv2.fillPoly(img_total, pts=[approx], color=255)
@@ -93,15 +80,47 @@ def carcass_coverage(image_path):
             percentage_coverage = 0
 
         img_fat = cv2.cvtColor(img_fat, cv2.COLOR_GRAY2BGR)
-        img_fat[np.where((img_fat != [0, 0, 0]).all(axis=2))] = [0, 255, 255]
+        img_fat_overlay = img_fat.copy()
+        img_fat_overlay[np.where((img_fat_overlay != [0, 0, 0]).all(axis=2))] = [0, 255, 255]
 
         img_total_output = cv2.cvtColor(img_total, cv2.COLOR_GRAY2BGR)
         _, img_total_output = cv2.threshold(img_total_output, 50, 255, cv2.THRESH_BINARY)
         img_total_output = img & img_total_output
-        overlay = cv2.addWeighted(img_total_output, 0.8, img_fat, 0.2, 0)
+        overlay = cv2.addWeighted(img_total_output, 0.8, img_fat_overlay, 0.2, 0)
         font = cv2.FONT_HERSHEY_SIMPLEX
         cov_text = "Fat Coverage: %2.2f%%" % percentage_coverage
         cv2.putText(overlay, cov_text, (260, 630), font, 0.6, (255, 255, 255), 2, cv2.LINE_AA)
+
+        if show_steps | save_steps:
+            img_total_range = img & cv2.cvtColor(closing_img_total_gray, cv2.COLOR_GRAY2BGR)
+            img_total_range_c1 = img_total_range.copy()
+            img_total_range_c2 = img_total_range.copy()
+            cv2.drawContours(img_total_range_c1, [selected_contours[0]], -1, (0, 255, 0), 3)
+            cv2.drawContours(img_total_range_c2, [approx], -1, (0, 255, 0), 3)
+
+            if show_steps:
+                cv2.imshow("1-img.JPG", img)
+                cv2.imshow("2-img_hsv.JPG", img_hsv)
+                cv2.imshow("3-mask_bg1.JPG", mask_bg1)
+                cv2.imshow("4-mask_total.JPG", mask_total)
+                cv2.imshow("5-img_total_gray.JPG", img_total_gray)
+                cv2.imshow("6-closing_img_total_gray.JPG", closing_img_total_gray)
+                cv2.imshow("7-img_total_range.JPG", img_total_range)
+                cv2.imshow("8-img_total_range_c1.JPG", img_total_range_c1)
+                cv2.imshow("9-img_total_range_c2.JPG", img_total_range_c2)
+                cv2.imshow("10-img_fat.JPG", img & img_fat)
+
+            if save_steps:
+                cv2.imwrite("1-img.JPG", img)
+                cv2.imwrite("2-img_hsv.JPG", img_hsv)
+                cv2.imwrite("3-mask_bg1.JPG", mask_bg1)
+                cv2.imwrite("4-mask_total.JPG", mask_total)
+                cv2.imwrite("5-img_total_gray.JPG", img_total_gray)
+                cv2.imwrite("6-closing_img_total_gray.JPG", closing_img_total_gray)
+                cv2.imwrite("7-img_total_range.JPG", img_total_range)
+                cv2.imwrite("8-img_total_range_c1.JPG", img_total_range_c1)
+                cv2.imwrite("9-img_total_range_c2.JPG", img_total_range_c2)
+                cv2.imwrite("10-img_fat.JPG", img & img_fat)
 
         return overlay
 
--- implementation.py
+++ implementation.py
@@ -4,30 +4,33 @@ import os
 import glob
 import shutil
 
-# Execute the algorithm for all the IMG images in the test set
-curr_dir_path = os.path.dirname(os.path.realpath(__file__))
-output_dir = curr_dir_path + "/output/"
-files = glob.glob(curr_dir_path + '/IMG/**/*.JPG', recursive=True)
-
-shutil.rmtree(output_dir, ignore_errors=True)
-os.makedirs(output_dir)
-
-for file in files:
-    print(os.path.basename(file))
-    out_img = ccov.carcass_coverage(file)
-    cv2.imwrite(output_dir + os.path.basename(file), out_img)
+# # Execute the algorithm for all the IMG images in the test set
+# curr_dir_path = os.path.dirname(os.path.realpath(__file__))
+# output_dir = curr_dir_path + "/output/"
+# files = glob.glob(curr_dir_path + '/IMG/**/*.JPG', recursive=True)
+#
+# shutil.rmtree(output_dir, ignore_errors=True)
+# os.makedirs(output_dir)
+#
+# for file in files:
+#     print(os.path.basename(file))
+#     out_img = ccov.carcass_coverage(file)
+#     cv2.imwrite(output_dir + os.path.basename(file), out_img)
 
 
 # IMPORTANT CASES - Testing purposes
 
-# cv2.imshow("img", ccov.carcass_coverage("AOL4/46/DSC01363.JPG"))
-# cv2.imshow("img", ccov.carcass_coverage("AOL4/46/DSC01362.JPG"))
-# cv2.imshow("img", ccov.carcass_coverage("AOL4/50/CIMG2561.JPG"))
-# cv2.imshow("img", ccov.carcass_coverage("AOL1/04/DSC01428.JPG"))
-# cv2.imshow("img", ccov.carcass_coverage("AOL3/17/DSC01340.JPG"))
-# cv2.imshow("img", ccov.carcass_coverage("AOL2/45/DSC01252.JPG"))
-# cv2.imshow("img", ccov.carcass_coverage("AOL3/31/DSC01416.JPG"))
-# cv2.imshow("img", ccov.carcass_coverage("AOL2/47/DSC01279.JPG"))
-# cv2.imshow("img", ccov.carcass_coverage("AOL4/59/DSC01360.JPG"))
-# cv2.imshow("img", ccov.carcass_coverage("AOL2/48/DSC01277.JPG"))
+# cv2.imshow("img", ccov.carcass_coverage("IMG/AOL4/46/DSC01363.JPG"))
+# cv2.imshow("img", ccov.carcass_coverage("IMG/AOL4/46/DSC01362.JPG"))
+# cv2.imshow("img", ccov.carcass_coverage("IMG/AOL4/50/CIMG2561.JPG"))
+# cv2.imshow("img", ccov.carcass_coverage("IMG/AOL1/04/DSC01428.JPG"))
+# cv2.imshow("img", ccov.carcass_coverage("IMG/AOL3/17/DSC01340.JPG"))
+# cv2.imshow("img", ccov.carcass_coverage("IMG/AOL2/45/DSC01252.JPG"))
+# cv2.imshow("img", ccov.carcass_coverage("IMG/AOL3/31/DSC01416.JPG"))
+# cv2.imshow("img", ccov.carcass_coverage("IMG/AOL2/47/DSC01279.JPG"))
+# cv2.imshow("img", ccov.carcass_coverage("IMG/AOL4/59/DSC01360.JPG"))
+# cv2.imshow("img", ccov.carcass_coverage("IMG/AOL2/48/DSC01277.JPG"))
+
+cv2.imshow("final", ccov.carcass_coverage("IMG/AOL1/05/DSC01226.JPG", save_steps=True))
+
 cv2.waitKey(0)
